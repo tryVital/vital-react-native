944cc5f3046a3afc5b4fdbe68bb99c14e2209ca1
Rui Peres <sauron.is.alive@gmail.com>
2022-12-13 15:16:24 +0000

    No need

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -11,7 +11,7 @@
 import React from 'react';
 
 import {VitalCore} from 'vital-core-react-native';
-import {VitalHealth, VitalResource, status} from 'vital-health-react-native';
+import {VitalHealth, VitalResource} from 'vital-health-react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {NativeBaseProvider} from 'native-base';
 import HomeScreen from './screens/HomeScreen';
