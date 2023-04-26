0ca5835c8e2664c945e6cc01b31e4429c314302a
Anders Ha <anders@tryvital.io>
2023-04-26 10:48:01 +0100

    Update RN Example to include BP & data posting

--- example/src/ExampleUseCases.tsx
+++ example/src/ExampleUseCases.tsx
@@ -1,6 +1,6 @@
-import { Brand, Cancellable, DeviceKind, DeviceModel, VitalDevicesManager } from "@tryvital/vital-devices-react-native";
+import { ProviderSlug, VitalCore } from "@tryvital/vital-core-react-native";
+import { Brand, Cancellable, DeviceKind, ScannedDevice, VitalDevicesManager } from "@tryvital/vital-devices-react-native";
 import { VitalHealth, VitalResource } from "@tryvital/vital-health-react-native";
-import { Platform } from "react-native";
 import { PERMISSIONS, requestMultiple } from "react-native-permissions";
 
 // VitalHealth example use case: Sync Activity data
@@ -26,55 +26,120 @@ export async function syncActivityData() {
 export async function readBLEGlucoseMeter(
     deviceManager: VitalDevicesManager
 ) {
+    let glucoseDeviceModel = VitalDevicesManager.supportedDevices
+        .find((model) => model.kind == DeviceKind.GlucoseMeter && model.brand == Brand.AccuChek);
 
-    let bleSimulator: DeviceModel = {
-        id: (Platform.OS == "ios" ? '$vital_ble_simulator$' : '_vital_ble_simulator_'),
-        name: 'Vital BLE Simulator',
-        brand: Brand.AccuChek,
-        kind: DeviceKind.GlucoseMeter,
-    };
-    
+    if (glucoseDeviceModel == undefined) {
+        throw Error("@@@ [readBLEGlucoseMeter] Cannot find the BLE device model.");
+    }
+
+    await checkBluetoothPermissions()
+
+    console.log("@@@ Start scanning for glucose device: " + glucoseDeviceModel.name)
+
+    var scanner: Cancellable | null
+    scanner = deviceManager.scanForDevice(glucoseDeviceModel, {
+        onDiscovered: (device) => {
+            console.log("@@@ Discovered glucose meter: " + device.name + " (id = " + device.id + ")")
+            scanner?.cancel()
+
+            readScannedGlucoseMeter(device, deviceManager)
+        },
+        onError: (error) => console.log(error)
+    })
+}
+
+async function readScannedGlucoseMeter(
+    device: ScannedDevice,
+    deviceManager: VitalDevicesManager
+) {
+    console.log("@@@ Start pairing device: " + device.name + " (id = " + device.id + ")")
+
+    await deviceManager.pairDevice(device.id)
+
+    console.log("@@@ Successfully paired device: " + device.name + " (id = " + device.id + ")")
+
+    console.log("@@@ Start reading from device: " + device.name + " (id = " + device.id + ")")
+
+    let samples = await deviceManager.readGlucoseMeter(device.id)
+
+    console.log("@@@ Read " + samples.length + " samples from device: " + device.name + " (id = " + device.id + ")")
+    console.log(samples)
+
+    await VitalCore.postTimeSeriesData(
+        { "type": "glucose", "samples": samples },
+        ProviderSlug.AccuchekBLE
+    )
+
+    return samples
+}
+
+// VitalDevice example use case: Read BLE Blood Pressure meter
+export async function readBLEBloodPressureMeter(
+    deviceManager: VitalDevicesManager
+) {
+    let bpDeviceModel = VitalDevicesManager.supportedDevices
+        .find((model) => model.kind == DeviceKind.BloodPressure && model.brand == Brand.Omron);
+
+    if (bpDeviceModel == undefined) {
+        throw Error("@@@ [readBLEBloodPressureMeter] Cannot find the BLE device model.");
+    }
+
+    await checkBluetoothPermissions()
+
+    console.log("@@@ Start scanning for blood pressure device: " + bpDeviceModel.name)
+
+    var scanner: Cancellable | null
+    scanner = deviceManager.scanForDevice(bpDeviceModel, {
+        onDiscovered: (device) => {
+            console.log("@@@ Discovered blood pressure meter: " + device.name + " (id = " + device.id + ")")
+            scanner?.cancel()
+
+            readScannedBloodPressureMeter(device, deviceManager)
+        },
+        onError: (error) => console.log(error)
+    })
+}
+
+async function readScannedBloodPressureMeter(
+    device: ScannedDevice,
+    deviceManager: VitalDevicesManager
+) {
+    console.log("@@@ Start pairing device: " + device.name + " (id = " + device.id + ")")
+
+    await deviceManager.pairDevice(device.id)
+
+    console.log("@@@ Successfully paired device: " + device.name + " (id = " + device.id + ")")
+
+    console.log("@@@ Start reading from device: " + device.name + " (id = " + device.id + ")")
+
+    let samples = await deviceManager.readBloodPressure(device.id)
+
+    console.log("@@@ Read " + samples.length + " samples from device: " + device.name + " (id = " + device.id + ")")
+    console.log(samples)
+
+    await VitalCore.postTimeSeriesData(
+        { "type": "blood_pressure", "samples": samples },
+        ProviderSlug.OmronBLE
+    )
+
+    return samples
+}
+
+async function checkBluetoothPermissions() {
     let statuses = await requestMultiple([
         PERMISSIONS.ANDROID.BLUETOOTH_SCAN,
         PERMISSIONS.ANDROID.BLUETOOTH_CONNECT,
         PERMISSIONS.IOS.BLUETOOTH_PERIPHERAL,
     ])
 
-    console.log(statuses);
-
     if (
     (statuses[PERMISSIONS.ANDROID.BLUETOOTH_SCAN] === 'granted' &&
         statuses[PERMISSIONS.ANDROID.BLUETOOTH_CONNECT] === 'granted') ||
     statuses[PERMISSIONS.IOS.BLUETOOTH_PERIPHERAL] === 'granted'
     ) {
-        console.log("@@@ Start scanning for device type: " + bleSimulator.name)
+        console.log("@@@ BLE permission granted: " + statuses)
     } else {
-        console.log("@@@ BLE permission not granted.")
+        throw Error("@@@ BLE permission not all granted; granted status = " + statuses)
     }
-    
-    var scanner: Cancellable | null
-    scanner = deviceManager.scanForDevice(
-        bleSimulator,
-        {
-        onDiscovered: (device) => {
-            console.log("@@@ Discovered device: " + device.name + " (id = " + device.id + ")")
-            scanner?.cancel()
-
-            console.log("@@@ Start pairing device: " + device.name + " (id = " + device.id + ")")
-            deviceManager.pairDevice(device.id)
-            .then(() => {
-                console.log("@@@ Successfully paired device: " + device.name + " (id = " + device.id + ")")
-                console.log("@@@ Start reading from device: " + device.name + " (id = " + device.id + ")")
-
-                return deviceManager.readGlucoseMeter(device.id)
-            })
-            .then((samples) => {
-                console.log("@@@ Read " + samples.length + " samples from device: " + device.name + " (id = " + device.id + ")")
-                console.log(samples)
-            })
-            .catch((error) => console.log(error))
-        },
-        onError: (error) => console.log(error)
-        }
-    )
 }
