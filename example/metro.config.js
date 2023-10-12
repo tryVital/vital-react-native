2f0a7e17a2c04b56d6fd84322690580ae89dbc4d
Anders Ha <anders@tryvital.io>
2023-10-12 15:08:52 +0100

    VIT-4706: Fix example app and local packages using different copies of RN packages

--- example/metro.config.js
+++ example/metro.config.js
@@ -5,11 +5,6 @@
  * @format
  */
 
-const exclusionList = require("metro-config/src/defaults/exclusionList");
-const { getMetroTools } = require("react-native-monorepo-tools");
-
-const monorepoMetroTools = getMetroTools();
-
 module.exports = {
   transformer: {
     getTransformOptions: async () => ({
@@ -18,10 +13,5 @@ module.exports = {
         inlineRequires: true,
       },
     }),
-  },  
-  watchFolders: monorepoMetroTools.watchFolders,
-  resolver: {
-    blockList: exclusionList(monorepoMetroTools.blockList),
-    extraNodeModules: monorepoMetroTools.extraNodeModules,
-  },
+  }
 };
