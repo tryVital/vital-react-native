ab34e6405ff0aca35c2f238490c25ad6c9f78de0
Filip Komorowski <filip@tryvital.io>
2024-09-26 14:58:01 +0200

    Bump Android SDK 3.2.4

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -55,8 +55,6 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
 
-    VitalHealthReactNativeModule.enableDebugLogging();
-
     // If you opted-in for the New Architecture, we enable the TurboModule system
     ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     SoLoader.init(this, /* native exopackage */ false);
