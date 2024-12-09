54af4d2208fd72a41b20edf9c588871df939503f
Anders Ha <anders@tryvital.io>
2024-12-09 19:58:09 +0000

    VIT-7941: Update Expo plugin to support HealthKit background delivery (#121)
    * Bundle app.plugin.js when publishing the packages
    * Add missing mods for Info.plist and entitlements.
    * Add missing mod for the AppDelegate `-didFinishLaunchingWithOptions:` message.

--- packages/vital-health-react-native/app.plugin.js
+++ packages/vital-health-react-native/app.plugin.js
@@ -2,31 +2,56 @@ const {
   withEntitlementsPlist,
   withInfoPlist,
   withAndroidManifest,
+  withAppDelegate,
 } = require('@expo/config-plugins');
 const { withPlugins } = require('@expo/config-plugins');
+const { addObjcImports, insertContentsInsideObjcFunctionBlock } = require('@expo/config-plugins/build/ios/codeMod');
 
 const HEALTH_SHARE = 'Allow $(PRODUCT_NAME) to check health info';
 
 const withHealthKit = (config, { healthSharePermission } = {}) => {
-  // Add permissions
+
   config = withInfoPlist(config, (config) => {
+
     config.modResults.NSHealthShareUsageDescription =
       healthSharePermission ||
       config.modResults.NSHealthShareUsageDescription ||
       HEALTH_SHARE;
 
+    const declaredModes = config.modResults.UIBackgroundModes ?? [];
+
+    if (!declaredModes.includes('processing')) {
+      config.modResults.UIBackgroundModes = [...declaredModes, 'processing'];
+    }
+
     return config;
   });
 
-  // Add entitlements. These are automatically synced when using EAS build for production apps.
   config = withEntitlementsPlist(config, (config) => {
     config.modResults['com.apple.developer.healthkit'] = true;
+    config.modResults['com.apple.developer.healthkit.background-delivery'] = true;
+
     if (
       !Array.isArray(config.modResults['com.apple.developer.healthkit.access'])
     ) {
       config.modResults['com.apple.developer.healthkit.access'] = [];
     }
 
+    return config;
+  });
+
+  config = withAppDelegate(config, (config) => {
+		let source = config.modResults.contents;
+
+    source = addObjcImports(source, ['"VitalHealthKitConfiguration.h"']);
+    source = insertContentsInsideObjcFunctionBlock(
+      source,
+      'application didFinishLaunchingWithOptions:',
+      '[VitalHealthKitConfiguration automaticConfiguration];',
+      { position: "head" },
+    );
+
+    config.modResults.contents = source;
     return config;
   });
 
