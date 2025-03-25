d6059ec18a6f1800a52e03d0d5e396f0a1c23e52
Anders Ha <anders@tryvital.io>
2025-03-25 12:36:11 +0000

    DEV-328: Include a HealthKitDataType string literal union

---
+++ packages/vital-health-react-native/src/healthkit.ts
@@ -0,0 +1,234 @@
+export type HealthKitDataType = (
+    "HKQuantityTypeIdentifierAppleSleepingWristTemperature" |
+    "HKQuantityTypeIdentifierBodyFatPercentage" |
+    "HKQuantityTypeIdentifierBodyMass" |
+    "HKQuantityTypeIdentifierBodyMassIndex" |
+    "HKQuantityTypeIdentifierElectrodermalActivity" |
+    "HKQuantityTypeIdentifierHeight" |
+    "HKQuantityTypeIdentifierLeanBodyMass" |
+    "HKQuantityTypeIdentifierWaistCircumference" |
+
+    "HKQuantityTypeIdentifierActiveEnergyBurned" |
+    "HKQuantityTypeIdentifierAppleExerciseTime" |
+    "HKQuantityTypeIdentifierAppleMoveTime" |
+    "HKQuantityTypeIdentifierAppleStandTime" |
+    "HKQuantityTypeIdentifierBasalEnergyBurned" |
+    "HKQuantityTypeIdentifierCrossCountrySkiingSpeed" |
+    "HKQuantityTypeIdentifierCyclingCadence" |
+    "HKQuantityTypeIdentifierCyclingFunctionalThresholdPower" |
+    "HKQuantityTypeIdentifierCyclingPower" |
+    "HKQuantityTypeIdentifierCyclingSpeed" |
+    "HKQuantityTypeIdentifierDistanceCrossCountrySkiing" |
+    "HKQuantityTypeIdentifierDistanceCycling" |
+    "HKQuantityTypeIdentifierDistanceDownhillSnowSports" |
+    "HKQuantityTypeIdentifierDistancePaddleSports" |
+    "HKQuantityTypeIdentifierDistanceRowing" |
+    "HKQuantityTypeIdentifierDistanceSkatingSports" |
+    "HKQuantityTypeIdentifierDistanceSwimming" |
+    "HKQuantityTypeIdentifierDistanceWalkingRunning" |
+    "HKQuantityTypeIdentifierDistanceWheelchair" |
+    "HKQuantityTypeIdentifierEstimatedWorkoutEffortScore" |
+    "HKQuantityTypeIdentifierFlightsClimbed" |
+    "HKQuantityTypeIdentifierNikeFuel" |
+    "HKQuantityTypeIdentifierPaddleSportsSpeed" |
+    "HKQuantityTypeIdentifierPhysicalEffort" |
+    "HKQuantityTypeIdentifierPushCount" |
+    "HKQuantityTypeIdentifierRowingSpeed" |
+    "HKQuantityTypeIdentifierRunningPower" |
+    "HKQuantityTypeIdentifierRunningSpeed" |
+    "HKQuantityTypeIdentifierStepCount" |
+    "HKQuantityTypeIdentifierSwimmingStrokeCount" |
+    "HKQuantityTypeIdentifierUnderwaterDepth" |
+    "HKQuantityTypeIdentifierWorkoutEffortScore" |
+
+    "HKQuantityTypeIdentifierEnvironmentalAudioExposure" |
+    "HKQuantityTypeIdentifierEnvironmentalSoundReduction" |
+    "HKQuantityTypeIdentifierHeadphoneAudioExposure" |
+
+    "HKQuantityTypeIdentifierAtrialFibrillationBurden" |
+    "HKQuantityTypeIdentifierHeartRate" |
+    "HKQuantityTypeIdentifierHeartRateRecoveryOneMinute" |
+    "HKQuantityTypeIdentifierHeartRateVariabilitySDNN" |
+    "HKQuantityTypeIdentifierPeripheralPerfusionIndex" |
+    "HKQuantityTypeIdentifierRestingHeartRate" |
+    "HKQuantityTypeIdentifierVO2Max" |
+    "HKQuantityTypeIdentifierWalkingHeartRateAverage" |
+
+    "HKQuantityTypeIdentifierAppleWalkingSteadiness" |
+    "HKQuantityTypeIdentifierRunningGroundContactTime" |
+    "HKQuantityTypeIdentifierRunningStrideLength" |
+    "HKQuantityTypeIdentifierRunningVerticalOscillation" |
+    "HKQuantityTypeIdentifierSixMinuteWalkTestDistance" |
+    "HKQuantityTypeIdentifierStairAscentSpeed" |
+    "HKQuantityTypeIdentifierStairDescentSpeed" |
+    "HKQuantityTypeIdentifierWalkingAsymmetryPercentage" |
+    "HKQuantityTypeIdentifierWalkingDoubleSupportPercentage" |
+    "HKQuantityTypeIdentifierWalkingSpeed" |
+    "HKQuantityTypeIdentifierWalkingStepLength" |
+
+    "HKQuantityTypeIdentifierDietaryBiotin" |
+    "HKQuantityTypeIdentifierDietaryCaffeine" |
+    "HKQuantityTypeIdentifierDietaryCalcium" |
+    "HKQuantityTypeIdentifierDietaryCarbohydrates" |
+    "HKQuantityTypeIdentifierDietaryChloride" |
+    "HKQuantityTypeIdentifierDietaryCholesterol" |
+    "HKQuantityTypeIdentifierDietaryChromium" |
+    "HKQuantityTypeIdentifierDietaryCopper" |
+    "HKQuantityTypeIdentifierDietaryEnergyConsumed" |
+    "HKQuantityTypeIdentifierDietaryFatMonounsaturated" |
+    "HKQuantityTypeIdentifierDietaryFatPolyunsaturated" |
+    "HKQuantityTypeIdentifierDietaryFatSaturated" |
+    "HKQuantityTypeIdentifierDietaryFatTotal" |
+    "HKQuantityTypeIdentifierDietaryFiber" |
+    "HKQuantityTypeIdentifierDietaryFolate" |
+    "HKQuantityTypeIdentifierDietaryIodine" |
+    "HKQuantityTypeIdentifierDietaryIron" |
+    "HKQuantityTypeIdentifierDietaryMagnesium" |
+    "HKQuantityTypeIdentifierDietaryManganese" |
+    "HKQuantityTypeIdentifierDietaryMolybdenum" |
+    "HKQuantityTypeIdentifierDietaryNiacin" |
+    "HKQuantityTypeIdentifierDietaryPantothenicAcid" |
+    "HKQuantityTypeIdentifierDietaryPhosphorus" |
+    "HKQuantityTypeIdentifierDietaryPotassium" |
+    "HKQuantityTypeIdentifierDietaryProtein" |
+    "HKQuantityTypeIdentifierDietaryRiboflavin" |
+    "HKQuantityTypeIdentifierDietarySelenium" |
+    "HKQuantityTypeIdentifierDietarySodium" |
+    "HKQuantityTypeIdentifierDietarySugar" |
+    "HKQuantityTypeIdentifierDietaryThiamin" |
+    "HKQuantityTypeIdentifierDietaryVitaminA" |
+    "HKQuantityTypeIdentifierDietaryVitaminB12" |
+    "HKQuantityTypeIdentifierDietaryVitaminB6" |
+    "HKQuantityTypeIdentifierDietaryVitaminC" |
+    "HKQuantityTypeIdentifierDietaryVitaminD" |
+    "HKQuantityTypeIdentifierDietaryVitaminE" |
+    "HKQuantityTypeIdentifierDietaryVitaminK" |
+    "HKQuantityTypeIdentifierDietaryWater" |
+    "HKQuantityTypeIdentifierDietaryZinc" |
+
+    "HKQuantityTypeIdentifierBloodAlcoholContent" |
+    "HKQuantityTypeIdentifierBloodPressureDiastolic" |
+    "HKQuantityTypeIdentifierBloodPressureSystolic" |
+    "HKQuantityTypeIdentifierInsulinDelivery" |
+    "HKQuantityTypeIdentifierNumberOfAlcoholicBeverages" |
+    "HKQuantityTypeIdentifierNumberOfTimesFallen" |
+    "HKQuantityTypeIdentifierTimeInDaylight" |
+    "HKQuantityTypeIdentifierUVExposure" |
+    "HKQuantityTypeIdentifierWaterTemperature" |
+
+    "HKQuantityTypeIdentifierBasalBodyTemperature" |
+
+    "HKQuantityTypeIdentifierAppleSleepingBreathingDisturbances" |
+    "HKQuantityTypeIdentifierForcedExpiratoryVolume1" |
+    "HKQuantityTypeIdentifierForcedVitalCapacity" |
+    "HKQuantityTypeIdentifierInhalerUsage" |
+    "HKQuantityTypeIdentifierOxygenSaturation" |
+    "HKQuantityTypeIdentifierPeakExpiratoryFlowRate" |
+    "HKQuantityTypeIdentifierRespiratoryRate" |
+
+    "HKQuantityTypeIdentifierBloodGlucose" |
+    "HKQuantityTypeIdentifierBodyTemperature" |
+
+    "HKCategoryTypeIdentifierAppleStandHour" |
+
+    "HKCategoryTypeIdentifierEnvironmentalAudioExposureEvent" |
+    "HKCategoryTypeIdentifierHeadphoneAudioExposureEvent" |
+
+    "HKCategoryTypeIdentifierHighHeartRateEvent" |
+    "HKCategoryTypeIdentifierIrregularHeartRhythmEvent" |
+    "HKCategoryTypeIdentifierLowCardioFitnessEvent" |
+    "HKCategoryTypeIdentifierLowHeartRateEvent" |
+
+    "HKCategoryTypeIdentifierMindfulSession" |
+
+    "HKCategoryTypeIdentifierAppleWalkingSteadinessEvent" |
+
+    "HKCategoryTypeIdentifierHandwashingEvent" |
+    "HKCategoryTypeIdentifierToothbrushingEvent" |
+
+    "HKCategoryTypeIdentifierBleedingAfterPregnancy" |
+    "HKCategoryTypeIdentifierBleedingDuringPregnancy" |
+    "HKCategoryTypeIdentifierCervicalMucusQuality" |
+    "HKCategoryTypeIdentifierContraceptive" |
+    "HKCategoryTypeIdentifierInfrequentMenstrualCycles" |
+    "HKCategoryTypeIdentifierIntermenstrualBleeding" |
+    "HKCategoryTypeIdentifierIrregularMenstrualCycles" |
+    "HKCategoryTypeIdentifierLactation" |
+    "HKCategoryTypeIdentifierMenstrualFlow" |
+    "HKCategoryTypeIdentifierOvulationTestResult" |
+    "HKCategoryTypeIdentifierPersistentIntermenstrualBleeding" |
+    "HKCategoryTypeIdentifierPregnancy" |
+    "HKCategoryTypeIdentifierPregnancyTestResult" |
+    "HKCategoryTypeIdentifierProgesteroneTestResult" |
+    "HKCategoryTypeIdentifierProlongedMenstrualPeriods" |
+    "HKCategoryTypeIdentifierSexualActivity" |
+
+    "HKCategoryTypeIdentifierSleepApneaEvent" |
+
+    "HKCategoryTypeIdentifierSleepAnalysis" |
+
+    "HKCategoryTypeIdentifierAbdominalCramps" |
+    "HKCategoryTypeIdentifierAcne" |
+    "HKCategoryTypeIdentifierAppetiteChanges" |
+    "HKCategoryTypeIdentifierBladderIncontinence" |
+    "HKCategoryTypeIdentifierBloating" |
+    "HKCategoryTypeIdentifierBreastPain" |
+    "HKCategoryTypeIdentifierChestTightnessOrPain" |
+    "HKCategoryTypeIdentifierChills" |
+    "HKCategoryTypeIdentifierConstipation" |
+    "HKCategoryTypeIdentifierCoughing" |
+    "HKCategoryTypeIdentifierDiarrhea" |
+    "HKCategoryTypeIdentifierDizziness" |
+    "HKCategoryTypeIdentifierDrySkin" |
+    "HKCategoryTypeIdentifierFainting" |
+    "HKCategoryTypeIdentifierFatigue" |
+    "HKCategoryTypeIdentifierFever" |
+    "HKCategoryTypeIdentifierGeneralizedBodyAche" |
+    "HKCategoryTypeIdentifierHairLoss" |
+    "HKCategoryTypeIdentifierHeadache" |
+    "HKCategoryTypeIdentifierHeartburn" |
+    "HKCategoryTypeIdentifierHotFlashes" |
+    "HKCategoryTypeIdentifierLossOfSmell" |
+    "HKCategoryTypeIdentifierLossOfTaste" |
+    "HKCategoryTypeIdentifierLowerBackPain" |
+    "HKCategoryTypeIdentifierMemoryLapse" |
+    "HKCategoryTypeIdentifierMoodChanges" |
+    "HKCategoryTypeIdentifierNausea" |
+    "HKCategoryTypeIdentifierNightSweats" |
+    "HKCategoryTypeIdentifierPelvicPain" |
+    "HKCategoryTypeIdentifierRapidPoundingOrFlutteringHeartbeat" |
+    "HKCategoryTypeIdentifierRunnyNose" |
+    "HKCategoryTypeIdentifierShortnessOfBreath" |
+    "HKCategoryTypeIdentifierSinusCongestion" |
+    "HKCategoryTypeIdentifierSkippedHeartbeat" |
+    "HKCategoryTypeIdentifierSleepChanges" |
+    "HKCategoryTypeIdentifierSoreThroat" |
+    "HKCategoryTypeIdentifierVaginalDryness" |
+    "HKCategoryTypeIdentifierVomiting" |
+    "HKCategoryTypeIdentifierWheezing" |
+
+    "HKCharacteristicTypeIdentifierActivityMoveMode" |
+    "HKCharacteristicTypeIdentifierBiologicalSex" |
+    "HKCharacteristicTypeIdentifierBloodType" |
+    "HKCharacteristicTypeIdentifierDateOfBirth" |
+    "HKCharacteristicTypeIdentifierFitzpatrickSkinType" |
+    "HKCharacteristicTypeIdentifierWheelchairUse" |
+
+    "HKCorrelationTypeIdentifierBloodPressure" |
+
+    "HKCorrelationTypeIdentifierFood" |
+
+    "HKDocumentTypeIdentifierCDA" |
+
+    "HKScoredAssessmentTypeIdentifierGAD7" |
+    "HKScoredAssessmentTypeIdentifierPHQ9" |
+
+    "HKWorkoutTypeIdentifier" |
+
+    "HKWorkoutRouteTypeIdentifier" |
+    "HKDataTypeIdentifierHeartbeatSeries" |
+
+    "HKVisionPrescriptionTypeIdentifier" |
+
+    "HKDataTypeIdentifierStateOfMind"
+);
