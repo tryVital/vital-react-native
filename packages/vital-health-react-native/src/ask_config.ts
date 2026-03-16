54a9b3ecbaea80fd5de58e278527b06f1524a136
Anders Ha <anders@junction.com>
2026-03-16 22:22:44 +0000

    apply prettier

--- packages/vital-health-react-native/src/ask_config.ts
+++ packages/vital-health-react-native/src/ask_config.ts
@@ -1,25 +1,25 @@
-import type { HealthKitDataType } from "./healthkit";
+import type { HealthKitDataType } from './healthkit';
 
 export interface AskConfigiOS {
-    type: "ios";
+  type: 'ios';
 
-    /**
-     * Extra HealthKit object types whose read permissions should be requested in addition to the needs of Junction Mobile SDK.
-     */
-    extraReadPermissions?: HealthKitDataType[];
-    /**
-     * Extra HealthKit sample types whose write permissions should be requested in addition to the needs of Junction Mobile SDK.
-     */
-    extraWritePermissions?: HealthKitDataType[];
-    /**
-     * If not undefined, only the specified data types would be requested. This applies to both SDK originated requests as well
-     * as extra permissions you specified above.
-     */
-    dataTypeAllowlist?: HealthKitDataType[];
+  /**
+   * Extra HealthKit object types whose read permissions should be requested in addition to the needs of Junction Mobile SDK.
+   */
+  extraReadPermissions?: HealthKitDataType[];
+  /**
+   * Extra HealthKit sample types whose write permissions should be requested in addition to the needs of Junction Mobile SDK.
+   */
+  extraWritePermissions?: HealthKitDataType[];
+  /**
+   * If not undefined, only the specified data types would be requested. This applies to both SDK originated requests as well
+   * as extra permissions you specified above.
+   */
+  dataTypeAllowlist?: HealthKitDataType[];
 }
 
 export interface AskConfigAndroid {
-    type: "android";
+  type: 'android';
 }
 
 export type AskConfig = AskConfigiOS | AskConfigAndroid;
