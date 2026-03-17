a1fcc7d5d7774906fc4b5c9fe86a3f7588e17073
Anders Ha <anders@junction.com>
2026-03-17 16:50:35 +0000

    Release 6.0.0-rc.1

--- scripts/syncpkg.js
+++ scripts/syncpkg.js
@@ -2,12 +2,18 @@
 
 const crypto = require('crypto');
 const fs = require('fs');
+const os = require('os');
 const path = require('path');
 const { execFileSync } = require('child_process');
 
 const rootDir = path.resolve(__dirname, '..');
 const archiveDir = path.join(rootDir, 'lib');
 const exampleDir = path.join(rootDir, 'example');
+const examplePackageJsonPath = path.join(exampleDir, 'package.json');
+const exampleLockfilePath = path.join(exampleDir, 'yarn.lock');
+const npmCacheDir =
+  process.env.NPM_CONFIG_CACHE ??
+  path.join(os.tmpdir(), 'vital-react-native-npm-cache');
 
 const packages = [
   {
@@ -32,8 +38,20 @@ function run(command, args, options = {}) {
   return execFileSync(command, args, {
     cwd: options.cwd ?? rootDir,
     encoding: options.encoding,
+    env: {
+      ...process.env,
+      ...(options.env ?? {}),
+    },
     stdio: options.stdio ?? 'inherit',
   });
+}
+
+function readJson(filePath) {
+  return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+}
+
+function writeJson(filePath, value) {
+  fs.writeFileSync(filePath, `${JSON.stringify(value, null, 2)}\n`);
 }
 
 function packWorkspace(pkg) {
@@ -43,6 +61,9 @@ function packWorkspace(pkg) {
     {
       cwd: pkg.dir,
       encoding: 'utf8',
+      env: {
+        NPM_CONFIG_CACHE: npmCacheDir,
+      },
       stdio: ['ignore', 'pipe', 'inherit'],
     }
   );
@@ -74,14 +95,100 @@ function addArchiveInfix(filename, version, infix) {
   return filename.replace(/\.tgz$/, `-${infix}.tgz`);
 }
 
+function readFileIfExists(filePath) {
+  if (!fs.existsSync(filePath)) {
+    return null;
+  }
+
+  return fs.readFileSync(filePath, 'utf8');
+}
+
+function restoreFile(filePath, contents) {
+  if (contents === null) {
+    fs.rmSync(filePath, { force: true });
+    return;
+  }
+
+  fs.writeFileSync(filePath, contents);
+}
+
+function archivePrefixForPackage(packageName) {
+  return `${packageName.replace(/^@/, '').replace(/\//g, '-')}-`;
+}
+
+function managedPackageNames() {
+  return new Set(packages.map((pkg) => pkg.name));
+}
+
+function managedDependencyPackageNames() {
+  const names = managedPackageNames();
+  const dependencyNames = new Set();
+
+  for (const pkg of packages) {
+    const packageJson = readJson(path.join(pkg.dir, 'package.json'));
+
+    for (const dependencyGroup of [
+      packageJson.dependencies,
+      packageJson.optionalDependencies,
+    ]) {
+      for (const dependencyName of Object.keys(dependencyGroup ?? {})) {
+        if (names.has(dependencyName)) {
+          dependencyNames.add(dependencyName);
+        }
+      }
+    }
+  }
+
+  return dependencyNames;
+}
+
+function syncExampleResolutions(specByPackageName) {
+  const examplePackageJson = readJson(examplePackageJsonPath);
+  const nextResolutions = { ...(examplePackageJson.resolutions ?? {}) };
+
+  for (const packageName of managedPackageNames()) {
+    delete nextResolutions[packageName];
+  }
+
+  for (const packageName of managedDependencyPackageNames()) {
+    nextResolutions[packageName] = specByPackageName[packageName];
+  }
+
+  if (Object.keys(nextResolutions).length === 0) {
+    delete examplePackageJson.resolutions;
+  } else {
+    examplePackageJson.resolutions = nextResolutions;
+  }
+
+  writeJson(examplePackageJsonPath, examplePackageJson);
+}
+
+function cleanupStaleArchives(currentArchiveFilenames) {
+  const keep = new Set(currentArchiveFilenames);
+
+  for (const entry of fs.readdirSync(archiveDir, { withFileTypes: true })) {
+    if (!entry.isFile() || !entry.name.endsWith('.tgz') || keep.has(entry.name)) {
+      continue;
+    }
+
+    if (!packages.some((pkg) => entry.name.startsWith(archivePrefixForPackage(pkg.name)))) {
+      continue;
+    }
+
+    fs.rmSync(path.join(archiveDir, entry.name), { force: true });
+  }
+}
+
 function main() {
   const archiveInfix = crypto.randomBytes(4).toString('hex');
+  const examplePackageJson = readFileIfExists(examplePackageJsonPath);
+  const exampleLockfile = readFileIfExists(exampleLockfilePath);
 
-  fs.rmSync(archiveDir, { recursive: true, force: true });
   fs.mkdirSync(archiveDir, { recursive: true });
 
   run('yarn', ['workspaces', 'run', 'build']);
 
+  const currentArchiveFilenames = [];
   const specs = packages.map((pkg) => {
     const archive = packWorkspace(pkg);
     const archivePath = path.join(archiveDir, archive.filename);
@@ -91,6 +198,7 @@ function main() {
     );
 
     fs.renameSync(archivePath, renamedArchivePath);
+    currentArchiveFilenames.push(path.basename(renamedArchivePath));
 
     const relativeArchivePath = toPosixPath(
       path.relative(exampleDir, renamedArchivePath)
@@ -98,8 +206,27 @@ function main() {
     return `file:${relativeArchivePath}`;
   });
 
-  run('yarn', ['--cwd', exampleDir, 'remove', ...packages.map((pkg) => pkg.name)]);
-  run('yarn', ['--cwd', exampleDir, 'add', ...specs, '--check-files']);
+  const specByPackageName = Object.fromEntries(
+    packages.map((pkg, index) => [pkg.name, specs[index]])
+  );
+
+  try {
+    syncExampleResolutions(specByPackageName);
+    run('yarn', [
+      '--cwd',
+      exampleDir,
+      '--non-interactive',
+      'add',
+      ...specs,
+      '--check-files',
+    ]);
+  } catch (error) {
+    restoreFile(examplePackageJsonPath, examplePackageJson);
+    restoreFile(exampleLockfilePath, exampleLockfile);
+    throw error;
+  }
+
+  cleanupStaleArchives(currentArchiveFilenames);
 }
 
 main();
